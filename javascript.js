93957feaf438bb7fc8be03ae172379ad67773d5a
LemonAir14 <33576812+LemonAir14@users.noreply.github.com>
2022-08-26 15:39:13 -0400

    capitalized player selection and made statement telling player and computer choice

--- javascript.js
+++ javascript.js
@@ -1,9 +1,16 @@
 let choice = ['Rock','Paper','Scissors']
 
-function computerchoice() {
+function getcomputerselection() {
     return choice[Math.floor(Math.random()*3)]
 }
-console.log(computerchoice())
+let computerselection = getcomputerselection()
+let playerselection = prompt ("Rock,Paper or Scissors?")
 
-let playerchoice = prompt ("Rock,Paper or Scissors?")
+function finalplayerselection(playerselection) {
+    return (playerselection.substring(0,1)).toUpperCase() + (playerselection.substring(1)).toLowerCase()
+}
+  console.log(`The Player Chose ${finalplayerselection(playerselection)} and the computer chose ${computerselection}`)
+ 
+
+
 
