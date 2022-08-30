3f5742ddd2067b5c68618b3577c9db3d006d6096
LemonAir14 <33576812+LemonAir14@users.noreply.github.com>
2022-08-30 14:09:45 -0400

    moved playerselection and computerselection nad other functions into playround function and looped it

--- javascript.js
+++ javascript.js
@@ -3,19 +3,18 @@ let choice = ['Rock','Paper','Scissors']
 function getcomputerselection() {
     return choice[Math.floor(Math.random()*3)]
 }
-let computerselection = getcomputerselection()
-let playerselection = prompt ("Rock,Paper or Scissors?")
 
 function finalplayerselection(playerselection) {
     return (playerselection.substring(0,1)).toUpperCase() + (playerselection.substring(1)).toLowerCase()
 }
+
+function playround(){
+  let playerselection = prompt ("Rock,Paper or Scissors?")
+  let computerselection = getcomputerselection()
   console.log(`The Player Chose ${finalplayerselection(playerselection)} and the computer chose ${computerselection}`)
-
-function playround(computerselection, playerselection){
   if (finalplayerselection(playerselection) == computerselection){return "This round is a draw!"} else if (finalplayerselection(playerselection) == "Rock") {return Rock()} else if (finalplayerselection(playerselection) == "Paper") {return Paper()} else if (finalplayerselection(playerselection) == "Scissors") {return Scissors()} else {return "You might've mispelled your choice."}
-}
-
- function Rock(){
+  
+  function Rock(){
     if (computerselection == "Scissors") {return "Rock beats Scissors so the Player wins!"} else if (computerselection == "Paper") {return "Rock loses to Paper so the Computer wins!"}
  }
 
@@ -27,4 +26,10 @@ function playround(computerselection, playerselection){
     if (computerselection == "Paper") {return "Scissors beats Paper so the Player wins!"} else if (computerselection == "Rock") {return "Scissors loses to Rock so the Computer wins!"}
  }
  
- console.log(playround(computerselection, playerselection))
+}
+
+ 
+ for (let i = 0; i < 5; i++){
+    console.log(playround())
+ }
+
